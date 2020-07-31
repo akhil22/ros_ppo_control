a53a500450aa21a80cb2fafad03bfdfe744d8314
akhil Nagariya <akhil.nagariya@gmail.com>
2020-07-31 01:00:41 -0500

    added control tester code

--- scripts/husky_ppo_control.py
+++ scripts/husky_ppo_control.py
@@ -21,10 +21,25 @@ class HuskyPPO:
         self.closest_dist = math.inf
         self.num_waypoints = 0
         self.horizon = 10
+    def set_waypoints_from_list(self, x_list, y_list, th_list, v_list):
+        for i in range(0, len(x_list)):
+            self.waypoints_list.append(np.array([x_list[i], y_list[i], th_list[i], v_list[i]]))
+        for i in range(0, len(self.waypoints_list) - 1):
+            xdiff = self.waypoints_list[i+1][0] - self.waypoints_list[i][0]
+            ydiff = self.waypoints_list[i+1][1] - self.waypoints_list[i][1]
+            self.waypoints_list[i][2] = self.zero_to_2pi(self.get_theta(xdiff, ydiff))
+        self.waypoints_list[i+1][2] = self.waypoints_list[i][2]
+        self.num_waypoints = i+2
     def zero_to_2pi(self, theta):
         if theta < 0:
             theta = 2*math.pi + theta
         elif theta > 2*math.pi:
+            theta = theta - 2*math.pi
+        return theta
+    def pi_to_pi(self, theta):
+        if theta < -math.pi:
+            theta = theta + 2*math.pi
+        elif theta > math.pi:
             theta = theta - 2*math.pi
         return theta
     def get_theta(self, xdiff, ydiff):
@@ -33,7 +48,7 @@ class HuskyPPO:
     def get_dist(self, waypoint, pose):
         xdiff = pose[0] - waypoint[0]
         ydiff = pose[1] - waypoint[1]
-        return math.sqrt(xdiff*xdiff, ydiff*ydiff)
+        return math.sqrt(xdiff*xdiff + ydiff*ydiff)
     def read_waypoint_file(self, filename):
         with open(filename) as csv_file:
             pos = csv.reader(csv_file, delimiter=',')
@@ -49,7 +64,7 @@ class HuskyPPO:
             self.num_waypoints = i+2
         pass
     def update_closest_idx(self, pose):
-        idx = closest_idx
+        idx = self.closest_idx
         for i in range(self.closest_idx, self.num_waypoints):
             dist = self.get_dist(self.waypoints_list[i], pose)
             if(dist <= self.closest_dist):
@@ -61,19 +76,34 @@ class HuskyPPO:
     def get_observation(self):
         obs = [0]*(self.horizon*4 + 2)
         pose = self.get_pose()
+        twist = self.get_twist()
         self.update_closest_idx(pose)
         j = 0
-        for i in closest_idx
-            r = get_dist(i, pose)
-            xdiff = i[0] - pose[0]
-            ydiff = i[1] - pose[1]
-            th = get_theta(xdiff, ydiff)
-            yaw_error = self.zero_to_2p(i[2] - pose[2])
-            vel = i[3]
-            obs[j] = r
-            obs[j+1] = th
-            obs[j+2] = yaw_error
-            obs[j+3] = vel
+        for i in range(0, self.horizon):
+            k = i + self.closest_idx
+            if k < self.num_waypoints:
+                r = self.get_dist(self.waypoints_list[k], pose)
+                xdiff = self.waypoints_list[k][0] - pose[0]
+                ydiff = self.waypoints_list[k][1] - pose[1]
+                th = self.get_theta(xdiff, ydiff)
+                vehicle_th = self.pi_to_pi(pose[2])
+                vehicle_th = -vehicle_th
+                vehicle_th = 2*math.pi - vehicle_th
+                yaw_error = self.pi_to_pi(self.waypoints_list[k][2] - vehicle_th)
+                vel = self.waypoints_list[k][3]
+                obs[j] = r
+                obs[j+1] = th
+                obs[j+2] = yaw_error
+                obs[j+3] = vel
+            else:
+                obs[j] = 0.
+                obs[j+1] = 0.
+                obs[j+2] = 0.
+                obs[j+3] = 0.
+            j = j+4
+        obs[j] = twist[0]
+        obs[j+1] = twist[1]
+        return obs
     def get_pose(self):
         self.pose_lock.acquire()
         current_pos = self.pose
@@ -92,10 +122,10 @@ class HuskyPPO:
         self.twist_lock.acquire()
         self.twist = twist
         self.twist_lock.release()
-    def odom_callback(self, data):
-        pass
-    def gps_callback(self, data):
-        pass
+    #def odom_callback(self, data):
+    #    pass
+    #def gps_callback(self, data):
+    #    pass
     def read_tf_frozen_graph(self, filepath):
         with tf.gfile.GFile(filepath, "rb") as f:
             self.graph_def = tf.GraphDef()
@@ -107,10 +137,7 @@ class HuskyPPO:
         feed_dict = {self.observation:observation}
         sess = tf.Session(graph = self.graph)
         op = self.graph.get_tensor_by_name("action:0")
-        print(sess.run(op, feed_dict))
-husky_ppo = HuskyPPO()
-husky_ppo.read_waypoint_file("waypoint.txt")
-print(husky_ppo.waypoints_list)
+        return sess.run(op, feed_dict)
 #print(husky_ppo.zero_to_2pi(-0.1))
 #print(husky_ppo.zero_to_2pi(2*math.pi + 0.5))
 '''husky_ppo = HuskyPPO()
