38b2daf7c1bfe47691305cb4670bda32780cedfb
Administrator <administrator@cpr-tamu11.cpr-tamu11>
2022-09-06 14:08:46 -0500

    added pytroch nodes and ppo controller

---
+++ scripts/ppo_control_pytroch.py
@@ -0,0 +1,178 @@
+import rospy
+#import torch
+from stable_baselines import PPO2
+import threading
+import numpy as np
+import utm
+import csv
+import math
+import gym
+class PPOControl:
+    def __init__(self):
+        self.twist_lock = threading.Lock()
+        self.pose_lock = threading.Lock()
+        self.path_lock = threading.Lock()
+        self.waypoints_list = []
+        self.pose = None
+        self.twist = None
+        self.model = None
+        self.graph_def = None
+        self.observation = None
+        self.closest_idx = 0
+        self.closest_dist = math.inf
+        self.num_waypoints = 0
+        self.horizon = 10
+        self.current_cross_track = 0
+    def set_waypoints_from_list(self, x_list, y_list, th_list, v_list):
+        self.path_lock.acquire()
+        self.waypoints_list = []
+        for i in range(0, len(x_list)):
+            self.waypoints_list.append(np.array([x_list[i], y_list[i], th_list[i], v_list[i]]))
+            if i > 0:
+                xdiff = self.waypoints_list[i][0] - self.waypoints_list[i-1][0]
+                ydiff = self.waypoints_list[i][1] - self.waypoints_list[i-1][1]
+                self.waypoints_list[i-1][2] = self.zero_to_2pi(self.get_theta(xdiff, ydiff))
+        self.waypoints_list[i-1][2] = self.waypoints_list[i-2][2]
+        self.num_waypoints = i
+        '''for i in range(0, len(self.waypoints_list) - 1):
+            xdiff = self.waypoints_list[i+1][0] - self.waypoints_list[i][0]
+            ydiff = self.waypoints_list[i+1][1] - self.waypoints_list[i][1]
+            self.waypoints_list[i][2] = self.zero_to_2pi(self.get_theta(xdiff, ydiff))
+        self.waypoints_list[i+1][2] = self.waypoints_list[i][2]
+        self.num_waypoints = i+2'''
+        self.path_lock.release()
+    def zero_to_2pi(self, theta):
+        if theta < 0:
+            theta = 2*math.pi + theta
+        elif theta > 2*math.pi:
+            theta = theta - 2*math.pi
+        return theta
+    def pi_to_pi(self, theta):
+        if theta < -math.pi:
+            theta = theta + 2*math.pi
+        elif theta > math.pi:
+            theta = theta - 2*math.pi
+        return theta
+    def get_theta(self, xdiff, ydiff):
+        theta = math.atan2(ydiff, xdiff)
+        return self.zero_to_2pi(theta)
+    def get_dist(self, waypoint, pose):
+        xdiff = pose[0] - waypoint[0]
+        ydiff = pose[1] - waypoint[1]
+        return math.sqrt(xdiff*xdiff + ydiff*ydiff)
+    def read_waypoint_file(self, filename):
+        with open(filename) as csv_file:
+            pos = csv.reader(csv_file, delimiter=',')
+            for row in pos:
+                #utm_cord = utm.from_latlon(float(row[0]), float(row[1]))
+                utm_cord = [float(row[0]), float(row[1])]
+                #phi = math.pi/4
+                phi = 0.
+                xcoord = utm_cord[0]*math.cos(phi) + utm_cord[1]*math.sin(phi)
+                ycoord = -utm_cord[0]*math.sin(phi) + utm_cord[1]*math.cos(phi)
+             #   self.waypoints_list.append(np.array([xcoord, ycoord, float(row[2]),float(row[3])]))
+                #self.waypoints_list.append(np.array([xcoord, ycoord, float(row[2]),2.5]))
+                self.waypoints_list.append(np.array([utm_cord[0], utm_cord[1], float(row[2]),float(row[3])]))
+               # self.waypoints_list.append(np.array([utm_cord[0], utm_cord[1], float(row[2]), 1.5]))
+            for i in range(0, len(self.waypoints_list) - 1):
+                xdiff = self.waypoints_list[i+1][0] - self.waypoints_list[i][0]
+                ydiff = self.waypoints_list[i+1][1] - self.waypoints_list[i][1]
+                self.waypoints_list[i][2] = self.zero_to_2pi(self.get_theta(xdiff, ydiff))
+            self.waypoints_list[i+1][2] = self.waypoints_list[i][2]
+            self.num_waypoints = i+2
+        pass
+    def update_closest_idx(self, pose):
+        idx = self.closest_idx
+        self.closest_dist = math.inf
+        for i in range(self.closest_idx, self.num_waypoints):
+            dist = self.get_dist(self.waypoints_list[i], pose)
+            if(dist <= self.closest_dist):
+                self.closest_dist = dist
+                idx = i
+            else:
+                break
+        self.closest_idx = idx
+    def get_observation(self):
+        self.path_lock.acquire()
+        obs = [0]*(self.horizon*4 + 2)
+        pose = self.get_pose()
+        twist = self.get_twist()
+        self.update_closest_idx(pose)
+        j = 0
+        for i in range(0, self.horizon):
+            k = i + self.closest_idx
+            if k < self.num_waypoints:
+                r = self.get_dist(self.waypoints_list[k], pose)
+                xdiff = self.waypoints_list[k][0] - pose[0]
+                ydiff = self.waypoints_list[k][1] - pose[1]
+                th = self.get_theta(xdiff, ydiff)
+                vehicle_th = self.zero_to_2pi(pose[2])
+                if i ==0:
+                    self.current_cross_track = r*math.sin(math.atan2(ydiff, xdiff) - self.waypoints_list[k][2])
+                #vehicle_th = -vehicle_th
+                #vehicle_th = 2*math.pi - vehicle_th
+                yaw_error = self.pi_to_pi(self.waypoints_list[k][2] - vehicle_th)
+                vel = self.waypoints_list[k][3]
+                obs[j] = r
+                obs[j+1] = self.pi_to_pi(th - vehicle_th)
+                obs[j+2] = yaw_error
+                obs[j+3] = vel - twist[0]
+            else:
+                obs[j] = 0.
+                obs[j+1] = 0.
+                obs[j+2] = 0.
+                obs[j+3] = 0.
+            j = j+4
+        self.path_lock.release()
+        obs[j] = twist[0]
+        obs[j+1] = twist[1]
+        return obs
+    def get_pose(self):
+        self.pose_lock.acquire()
+        current_pos = self.pose
+        self.pose_lock.release()
+        return current_pos
+    def get_twist(self):
+        self.twist_lock.acquire()
+        current_twist = self.twist
+        self.twist_lock.release()
+        return current_twist
+    def set_pose(self, pose):
+        self.pose_lock.acquire()
+        self.pose = pose
+        self.pose_lock.release()
+    def set_twist(self, twist):
+        self.twist_lock.acquire()
+        self.twist = twist
+        self.twist_lock.release()
+    #def odom_callback(self, data):
+    #    pass
+    #def gps_callback(self, data):
+    #    pass
+    def read_ppo_policy(self, filepath):
+        env = gym.make("CartPole-v1")
+        self.model = PPO2('MlpPolicy', env, verbose=1);
+        self.model = self.model.load(filepath);
+    def read_tf_frozen_graph(self, filepath):
+        with tf.gfile.GFile(filepath, "rb") as f:
+            self.graph_def = tf.GraphDef()
+            self.graph_def.ParseFromString(f.read())
+        with tf.Graph().as_default() as self.graph:
+            tf.import_graph_def(self.graph_def, name="")
+        self.observation = self.graph.get_tensor_by_name("vector_observation:0")
+    def get_control(self, observation):
+        feed_dict = {self.observation:observation}
+        sess = tf.Session(graph = self.graph)
+        op = self.graph.get_tensor_by_name("action:0")
+        return sess.run(op, feed_dict)
+    def get_ppo_control(self, observation):
+        action, _states = self.model.predict(observation, deterministic=True)
+        return [action]
+#print(husky_ppo.zero_to_2pi(-0.1))
+#print(husky_ppo.zero_to_2pi(2*math.pi + 0.5))
+'''husky_ppo = HuskyPPO()
+husky_ppo.read_tf_frozen_graph("/home/sai/hdd1/ml-master/ml-agents/config/ppo/results/wlong_path19/3DBall/dogs-cats-model.pb")
+observation = np.random.rand(1,42)
+husky_ppo.get_control(observation)
+husky_ppo.get_control(observation)
+husky_ppo.get_control(observation)'''
---
+++ scripts/warthog_ppo_node_pytorch.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python
+import rospy
+from ppo_control_tf import PPOControl
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from nav_msgs.msg import Path
+#from ilqr.msg import ins
+#import tensorflow as tf 
+from pyquaternion import Quaternion as qut
+import utm
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import rospkg
+def zero_to_2pi(theta):
+    if theta < 0:
+        theta = 2*math.pi + theta
+    elif theta > 2*math.pi:
+        theta = theta - 2*math.pi
+    return theta
+'''def zero_to_2pi(theta):
+    if theta > math.pi:
+        theta = theta - 2*math.pi
+    elif theta > -math.pi:
+        theta = theta + 2*math.pi
+    return theta'''
+def simulate_warthog(X, v, w, dt):
+    xcurr = X[0] + v*math.cos(X[2])*dt
+    ycurr = X[1] + v*math.sin(X[2])*dt
+    thcurr = zero_to_2pi(X[2]+w*dt)
+    #thcurr = X[2]+w*dt
+    #print(v,w)
+    return [xcurr, ycurr, thcurr]
+class HuskyPPONode:
+    def __init__(self):
+        self.warthog_ppo = PPOControl()
+        vel_topic = rospy.get_param('~vel_topic', 'warthog_velocity_controller/cmd_vel')
+        twist_odom_topic = rospy.get_param('~odom_topic', '/warthog_velocity_controller/odom')
+        #pose_odom_topic = rospy.get_param('~odom_topic2', '/odometry/filtered')
+        #pose_odom_topic = rospy.get_param('~odom_topic2', '/aft_mapped_to_init_high_frec')
+        pose_odom_topic = rospy.get_param('~odom_topic2', '/aft_mapped_to_init_high_frec')
+        #pose_odom_topic = rospy.get_param('~odom_topic2', '/odometry/filtered2')
+        ins_topic = rospy.get_param('~ins_topic', 'vectronav/fix')
+        path_topic = rospy.get_param('~path_topic', '/local_planning/path/final_trajectory')
+        #path_topic = rospy.get_param('~path_topic', '/local_planning/path/optimized_a_star_path')
+        #frozen_graph_path = rospy.get_param('~frozen_graph_path', "/home/sai/hdd1/ml-master/ml-agents/config/ppo/results/wlong_path54/3DBall/frozen_graph_def.pb")
+        rospack = rospkg.RosPack()
+        #pkg_path = rospack.get_path('ros_ppo_control')
+        pkg_path = "./"
+        frozen_graph_path = rospy.get_param('~frozen_graph_path', pkg_path + "/policies/wlong_path54/3DBall/frozen_graph_def.pb")
+        #waypoint_file_path = rospy.get_param('~waypoint_file_path', "unity_waypoints_bkp.txt")
+        waypoint_file_path = rospy.get_param('~waypoint_file_path', pkg_path + "/scripts/waypoints2.txt")
+        #self.warthog_ppo.read_tf_frozen_graph(frozen_graph_path)
+        #self.warthog_ppo.read_ppo_policy('/home/administrator/warthog_rl_alien/policy/vel_weight9_stable_delayed9')
+        #self.warthog_ppo.read_ppo_policy('/home/administrator/Downloads/warthog_rl/policy/vel_weight9_stable_delayed9')
+        #self.warthog_ppo.read_ppo_policy('/home/administrator/Downloads/warthog_rl/policy/vel_weight8_stable9')
+        #self.warthog_ppo.read_ppo_policy('/home/administrator/Downloads/warthog/warthog_rl/policy/combine_trained')
+        self.warthog_ppo.read_ppo_policy('/home/administrator/Downloads/warthog/warthog_rl/policy/combine_trained_may8_300')
+        #self.warthog_ppo.read_ppo_policy('/home/administrator/warthog_rl_alien/policy/vel_weight9_stable9')
+        #self.warthog_ppo.read_ppo_policy('./model2')
+        #self.warthog_ppo.read_waypoint_file(waypoint_file_path)
+        self.twist_pub = rospy.Publisher(vel_topic, Twist, queue_size = 10)
+        rospy.Subscriber(twist_odom_topic, Odometry, self.twist_odom_cb)
+        rospy.Subscriber(pose_odom_topic, Odometry, self.pose_odom_cb)
+        rospy.Subscriber(path_topic, Path, self.path_cb, queue_size = 1)
+        self.got_twist = False
+        self.got_odom = False
+        self.got_path = False
+        #rospy.Subscriber(ins_topic, ins, self.ins_cb)
+        show_path = False
+        if show_path:
+            self.cx = [i[0] for i in self.warthog_ppo.waypoints_list]
+            self.cy = [i[1] for i in self.warthog_ppo.waypoints_list]
+            print(self.warthog_ppo.waypoints_list)
+            plt.plot(self.cx, self.cy, '+b')
+            plt.show()
+    def twist_odom_cb(self, data):
+        #return
+        #if self.got_twist:
+            #return
+        v = data.twist.twist.linear.x
+        rospy.logwarn("getting twist")
+        w = data.twist.twist.angular.z
+        self.warthog_ppo.set_twist([v, w])
+        if not self.got_twist:
+            self.got_twist = True
+    def path_cb(self, path):
+        #if self.got_path:
+            #return
+        x_list = []
+        y_list = []
+        th_list = []
+        v_list = []
+        i = 0
+        for pose in path.poses:
+            x_list.append(pose.pose.position.x)
+            y_list.append(pose.pose.position.y)
+            th_list.append(pose.pose.position.x)
+            v_list.append(pose.pose.position.z)
+            #v_list.append(2.5)
+            i = i+1
+        x_list = x_list[:-1]
+        y_list = y_list[:-1]
+        th_list = th_list[:-1]
+        v_list = v_list[:-1]
+        '''for j in range(0,30):
+            x_list.append(x_list[i-2 + j] + x_list[i-3] - x_list[i-4])
+            y_list.append(y_list[i-2 + j] + y_list[i-3] - y_list[i-4])
+            th_list.append(0)
+            v_list.append(0)'''
+        self.warthog_ppo.set_waypoints_from_list(x_list, y_list, th_list, v_list)
+        if not self.got_path:
+            self.got_path = True
+    def pose_odom_cb(self, data):
+        #self.got_odom = True
+        #return
+        #if self.got_odom:
+            #return
+        x = data.pose.pose.position.x 
+        y = data.pose.pose.position.y
+        temp_y = data.pose.pose.orientation.z
+        temp_x = data.pose.pose.orientation.w
+        print("z: ", temp_y)
+        print("w: ", temp_x)
+        quat = (temp_x, 0, 0, temp_y)
+        myqut = qut(quat)
+        print("without sign: ",myqut.radians*180/math.pi)
+        th = myqut.radians*np.sign(temp_y)
+        print("with sign: ",th*180/math.pi)
+        #th = 2*math.atan2(temp_y, temp_x)*180/math.pi
+        #th = data.pose.covariance[1]
+        self.warthog_ppo.set_pose([x, y, th])
+        if not self.got_odom:
+            self.got_odom = True
+    def ins_cb(self, data):
+        lat = data.LLA.x
+        lon = data.LLA.y
+        utm_cord = utm.from_latlon(lat, lon)
+        self.warthog_ppo.set_pose([utm_cord[0], utm_cord[1]])
+
+def main():
+    rospy.init_node('warthog_ppo_node')
+    warthog_ppo_node = HuskyPPONode()
+    rate = rospy.Rate(30)
+    do_sim = rospy.get_param("~do_sim", True)
+    #do_sim = True
+    do_sim = False
+    #warthog_ppo_node.got_path = True
+    if do_sim:
+        r = rospy.Rate(1)
+        while(not rospy.is_shutdown() and (not warthog_ppo_node.got_path)):
+            rospy.logwarn("not getting path will try again")
+            r.sleep()
+        start_idx = 0
+        xinit = warthog_ppo_node.warthog_ppo.waypoints_list[start_idx][0] + 0.1
+        yinit = warthog_ppo_node.warthog_ppo.waypoints_list[start_idx][1] + 0.1
+        thinit = warthog_ppo_node.warthog_ppo.waypoints_list[start_idx][2]
+        warthog_ppo_node.warthog_ppo.set_pose([xinit, yinit, thinit])
+       # warthog_ppo_node.warthog_ppo.set_pose([132.180, -78.957, 160*math.pi/180.])
+        #warthog_ppo_node.warthog_ppo.set_pose([7.54831069e+05, 3.39048552e+06, 5.53962977e+00])
+        #warthog_ppo_node.warthog_ppo.set_pose([0. , 0., 5.53962977e+00])
+        warthog_ppo_node.warthog_ppo.set_twist([0., 0.])
+        x_pose = []
+        y_pose = []
+        for i in range(0, 100):
+            tstart = rospy.get_rostime()
+            obs = warthog_ppo_node.warthog_ppo.get_observation()
+            #twist = warthog_ppo_node.warthog_ppo.get_control(np.array(obs).reshape(1,42))
+            twist = warthog_ppo_node.warthog_ppo.get_ppo_control(np.array(obs).reshape(1,42))[0]
+            #v = np.clip(twist[0][0], 0, 1) * 2.0
+            #w = np.clip(twist[0][1], -1, 1) * 2.5
+            v = np.clip(twist[0][0], 0, 1)*4
+            w = np.clip(twist[0][1], -1, 1)*2.5
+            current_pose = simulate_warthog(warthog_ppo_node.warthog_ppo.get_pose(), v, w, 0.05)
+            warthog_ppo_node.warthog_ppo.set_pose(current_pose)
+            warthog_ppo_node.warthog_ppo.set_twist([v, w])
+            x_pose.append(current_pose[0])
+            y_pose.append(current_pose[1])
+            #print(v,w)
+            print(current_pose)
+            delta = (rospy.get_rostime() - tstart).to_sec()
+            logstring = "getting v= " + str(v) +" getting w= "+ str(w) + " time delta = " + str(delta)
+            rospy.loginfo(logstring)
+        warthog_ppo_node.warthog_ppo.path_lock.acquire()
+        #cx = [i[0] for i in warthog_ppo_node.warthog_ppo.waypoints_list]
+        #cy = [i[1] for i in warthog_ppo_node.warthog_ppo.waypoints_list]
+        warthog_ppo_node.warthog_ppo.path_lock.release()
+        #plt.plot(cx, cy, '+b')
+        plt.plot(x_pose, y_pose, '+g')
+        plt.show()
+    else:
+        x_pose = []
+        y_pose = []
+        start_idx = -1
+        v_rec = []
+        w_rec = []
+        v_act = []
+        w_act = []
+        cross_err = []
+        while not rospy.is_shutdown():
+            temp_pose=[0,0,0]
+            tstart = rospy.get_rostime()
+            if not (warthog_ppo_node.got_odom and warthog_ppo_node.got_path and warthog_ppo_node.got_twist):
+                if not warthog_ppo_node.got_odom:
+                    rospy.logwarn("Not Receiving Odometry")
+                if not warthog_ppo_node.got_twist:
+                    rospy.logwarn("Not Receiving Twist")
+                if not warthog_ppo_node.got_path:
+                    rospy.logwarn("Not Receiving Path")
+                continue
+            if start_idx == -1:
+                sim_run = False
+                if sim_run == True:
+                    start_idx = 1
+                    xinit = warthog_ppo_node.warthog_ppo.waypoints_list[start_idx][0] + 0.05
+                    yinit = warthog_ppo_node.warthog_ppo.waypoints_list[start_idx][1] + 0.05
+                    thinit = warthog_ppo_node.warthog_ppo.waypoints_list[start_idx][2]
+                    warthog_ppo_node.warthog_ppo.set_pose([xinit, yinit, thinit])
+            obs = warthog_ppo_node.warthog_ppo.get_observation()
+            twist = warthog_ppo_node.warthog_ppo.get_ppo_control(np.array(obs).reshape(1,42))
+            #v = np.clip(twist[0][0], 0, 1) * 2.0
+            closest_idx = warthog_ppo_node.warthog_ppo.closest_idx
+            #v = warthog_ppo_node.warthog_ppo.waypoints_list[closest_idx][3]
+            #w = np.clip(twist[0][1], -1, 1) * 2.5
+            twist = twist[0]
+            v = np.clip(twist[0][0], 0, 1)*4.0
+            w = np.clip(twist[0][1], -1, 1)*2.5
+            v_rec.append(v)
+            w_rec.append(w)
+            cross_err.append(warthog_ppo_node.warthog_ppo.current_cross_track)
+            if sim_run:
+                current_pose = simulate_warthog(warthog_ppo_node.warthog_ppo.get_pose(), v, w, 0.05)
+                warthog_ppo_node.warthog_ppo.set_pose(current_pose)
+                warthog_ppo_node.warthog_ppo.set_twist([v, w])
+            twist_msg = Twist()
+            twist_msg.linear.x =v
+            twist_msg.angular.z =w
+            warthog_ppo_node.twist_pub.publish(twist_msg)
+            delta = (rospy.get_rostime() - tstart).to_sec()
+            logstring = "getting v= " + str(v) +" getting w= "+ str(w) + " time delta = " + str(delta) + "theta = " + str(0.1)
+            rospy.logwarn(logstring)
+            if sim_run:
+                x_pose.append(current_pose[0])
+                y_pose.append(current_pose[1])
+            rate.sleep()
+            temp_pose = warthog_ppo_node.warthog_ppo.get_pose()
+            temp_twist = warthog_ppo_node.warthog_ppo.get_twist()
+            x_pose.append(temp_pose[0])
+            y_pose.append(temp_pose[1])
+            v_act.append(temp_twist[0])
+            w_act.append(temp_twist[1])
+        warthog_ppo_node.warthog_ppo.path_lock.acquire()
+        cx = [i[0] for i in warthog_ppo_node.warthog_ppo.waypoints_list]
+        cy = [i[1] for i in warthog_ppo_node.warthog_ppo.waypoints_list]
+        warthog_ppo_node.warthog_ppo.path_lock.release()
+        plt.figure(1)
+        plt.plot(v_rec, 'g', label='controller commanded v')
+        plt.plot(v_act, 'r', label='warthog actual v')
+        plt.legend()
+        plt.title("linear velocity plots")
+        plt.figure(2)
+        plt.plot(w_rec, 'g', label='controller commanded w')
+        plt.plot(w_act, 'r', label='warthog actual w')
+        plt.legend()
+        plt.title("angular velocity plots")
+        plt.figure(3)
+        #plt.show()
+        plt.plot(cx, cy, '+b', label='waypoints')
+        x_ = temp_pose[0]
+        y_ = temp_pose[1]
+        th_ = temp_pose[2]
+        plt.arrow(x_,y_, 2*math.cos(th_), 2*math.sin(th_), head_length=.5, head_width=.2)
+        print(cx)
+        print("diff")
+        print(cy)
+        plt.plot(x_pose, y_pose, '+g', label='controller trajectory')
+        plt.xlim(x_-30, x_ +30)
+        plt.ylim(y_-30, y_ +30)
+        plt.title("Ground truth vs Controller response")
+        plt.legend()
+        plt.figure(4)
+        plt.plot(cross_err, '+r', label='crosstrack error')
+        plt.legend()
+        plt.title('Crosstrack error with Time')
+        plt.show()
+if __name__=='__main__':
+    main()
